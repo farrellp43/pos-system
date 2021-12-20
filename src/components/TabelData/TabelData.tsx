a44ed40891e94eb54542ad728f322ddd3933b142
farrellp43 <farrellp43@gmail.com>
2021-12-20 14:34:02 +0700

    added modal on TabelData

--- src/components/TabelData/TabelData.tsx
+++ src/components/TabelData/TabelData.tsx
@@ -1,156 +1,277 @@
-import React, { useState } from 'react';
-import { DataGrid, GridColDef, GridToolbarExport, GridToolbar, GridToolbarDensitySelector, GridToolbarFilterButton } from '@mui/x-data-grid';
-import { Box, IconButton, TextField } from '@mui/material';
-import ClearIcon from '@mui/icons-material/Clear';
-import SearchIcon from '@mui/icons-material/Search';
-import { AnyRecord } from 'dns';
+import React from "react";
+import { DataGrid, GridColDef, GridToolbarExport } from "@mui/x-data-grid";
+import { Box, Button, IconButton, TextField } from "@mui/material";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
+import ClearIcon from "@mui/icons-material/Clear";
+import SearchIcon from "@mui/icons-material/Search";
+import AddIcon from "@mui/icons-material/Add";
 
 function escapeRegExp(value: string): string {
-    return value.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
+  return value.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
 }
 
-interface ITabelDataProps {
+interface ITabelDataProps {}
 
-}
-
-interface Customers {
-    id: number;
-    firstName: string;
-    lastName: string;
-    age: number;
-    email: string;
+interface IStokBarang {
+  id: number;
+  namaBarang: string;
+  harga: number;
+  jumlahStok: number;
+  SKU: string;
 }
 
 interface QuickSearchToolbarProps {
-    clearSearch: () => void;
-    onChange: () => void;
-    value: string;
+  clearSearch: () => void;
+  onChange: () => void;
+  value: string;
 }
 
 function QuickSearchToolbar(props: QuickSearchToolbarProps) {
-    return (
-        <Box
-            sx={{
-                p: 0.5,
-                pb: 0,
-                justifyContent: 'space-between',
-                display: 'flex',
-                alignItems: 'flex-start',
-                flexWrap: 'wrap',
-            }}
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <Box
+      sx={{
+        p: 0.5,
+        pb: 0,
+        justifyContent: "space-between",
+        display: "flex",
+        alignItems: "flex-start",
+        flexWrap: "wrap",
+      }}
+    >
+      <div>
+        <Button
+          variant="text"
+          size="small"
+          startIcon={<AddIcon />}
+          onClick={handleClickOpen}
         >
-            <div>
-                <GridToolbarFilterButton />
-                <GridToolbarDensitySelector />
-                <GridToolbarExport />
-            </div>
+          Add
+        </Button>
+        <Dialog
+          maxWidth="md"
+          fullWidth={true}
+          open={open}
+          onClose={handleClose}
+        >
+          <DialogTitle>Add</DialogTitle>
+          <DialogContent dividers>
             <TextField
-                variant="standard"
-                value={props.value}
-                onChange={props.onChange}
-                placeholder="Search…"
-                InputProps={{
-                    startAdornment: <SearchIcon fontSize="small" />,
-                    endAdornment: (
-                        <IconButton
-                            title="Clear"
-                            aria-label="Clear"
-                            size="small"
-                            style={{ visibility: props.value ? 'visible' : 'hidden' }}
-                            onClick={props.clearSearch}
-                        >
-                            <ClearIcon fontSize="small" />
-                        </IconButton>
-                    ),
-                }}
-                sx={{
-                    width: {
-                        xs: 1,
-                        sm: 'auto',
-                    },
-                    m: (theme) => theme.spacing(1, 0.5, 1.5),
-                    '& .MuiSvgIcon-root': {
-                        mr: 0.5,
-                    },
-                    '& .MuiInput-underline:before': {
-                        borderBottom: 1,
-                        borderColor: 'divider',
-                    },
-                }}
+              autoFocus
+              margin="dense"
+              id="namaBarang"
+              label="Nama Barang"
+              type="namaBarang"
+              fullWidth
+              variant="outlined"
             />
-        </Box>
-    );
+            <TextField
+              margin="dense"
+              id="harga"
+              label="Harga"
+              type="number"
+              fullWidth
+              variant="outlined"
+            />
+            <TextField
+              margin="dense"
+              id="jumlahStok"
+              label="Jumlah Stok"
+              type="number"
+              fullWidth
+              variant="outlined"
+            />
+            <TextField
+              margin="dense"
+              id="SKU"
+              label="SKU"
+              type="SKU"
+              fullWidth
+              variant="outlined"
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose}>Cancel</Button>
+            <Button onClick={handleClose}>Confirm</Button>
+          </DialogActions>
+        </Dialog>
+        <GridToolbarExport />
+      </div>
+      <TextField
+        variant="outlined"
+        value={props.value}
+        onChange={props.onChange}
+        placeholder="Search…"
+        InputProps={{
+          startAdornment: <SearchIcon fontSize="small" />,
+          endAdornment: (
+            <IconButton
+              title="Clear"
+              aria-label="Clear"
+              size="small"
+              style={{ visibility: props.value ? "visible" : "hidden" }}
+              onClick={props.clearSearch}
+            >
+              <ClearIcon fontSize="small" />
+            </IconButton>
+          ),
+        }}
+        sx={{
+          width: {
+            xs: 1,
+            sm: "auto",
+          },
+          m: (theme) => theme.spacing(1, 0.5, 1.5),
+          "& .MuiSvgIcon-root": {
+            mr: 0.5,
+          },
+          "& .MuiInput-underline:before": {
+            borderBottom: 1,
+            borderColor: "divider",
+          },
+        }}
+      />
+    </Box>
+  );
 }
 
 const columns: GridColDef[] = [
-    { field: 'id', headerName: 'ID', flex: 0.3 },
-    { field: 'firstName', headerName: 'First name', flex: 1, editable: true },
-    { field: 'lastName', headerName: 'Last name', flex: 1, editable: true },
-    {
-        field: 'age',
-        headerName: 'Age',
-        type: 'number',
-        flex: 0.3,
-        editable: true
-    },
-    {
-        field: 'email',
-        headerName: 'E-Mail',
-        flex: 1,
-        editable: true
-    },
+  { field: "id", headerName: "ID", flex: 0.3 },
+  { field: "namaBarang", headerName: "Nama Barang", flex: 0.6 },
+  {
+    field: "harga",
+    headerName: "Harga",
+    type: "number",
+    flex: 0.6,
+  },
+  {
+    field: "jumlahStok",
+    headerName: "Stok",
+    type: "number",
+    flex: 0.3,
+  },
+  {
+    field: "SKU",
+    headerName: "SKU",
+    flex: 1,
+  },
 ];
 
-const originalRows: Customers[] = [
-    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35, email: 'JSnow@example.com' },
-    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42, email: 'CLannister@example.com' },
-    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45, email: 'JLannister@example.com' },
-    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16, email: 'AStark@example.com' },
-    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: 999, email: 'DTargaryen@example.com' },
-    { id: 6, lastName: 'Melisandre', firstName: 'Fire', age: 150, email: 'Melisandre@example.com' },
-    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44, email: 'FClifford@example.com' },
-    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36, email: 'RFrances@example.com' },
-    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65, email: 'HRoxie@example.com' },
+const originalRows: IStokBarang[] = [
+  {
+    id: 1,
+    harga: 10000,
+    namaBarang: "Jon",
+    jumlahStok: 35,
+    SKU: "JSnow@example.com",
+  },
+  {
+    id: 2,
+    harga: 10000,
+    namaBarang: "Cersei",
+    jumlahStok: 42,
+    SKU: "CLannister@example.com",
+  },
+  {
+    id: 3,
+    harga: 10000,
+    namaBarang: "Jaime",
+    jumlahStok: 45,
+    SKU: "JLannister@example.com",
+  },
+  {
+    id: 4,
+    harga: 10000,
+    namaBarang: "Arya",
+    jumlahStok: 16,
+    SKU: "AStark@example.com",
+  },
+  {
+    id: 5,
+    harga: 10000,
+    namaBarang: "Daenerys",
+    jumlahStok: 999,
+    SKU: "DTargaryen@example.com",
+  },
+  {
+    id: 6,
+    harga: 10000,
+    namaBarang: "Fire",
+    jumlahStok: 150,
+    SKU: "Melisandre@example.com",
+  },
+  {
+    id: 7,
+    harga: 10000,
+    namaBarang: "Ferrara",
+    jumlahStok: 44,
+    SKU: "FClifford@example.com",
+  },
+  {
+    id: 8,
+    harga: 10000,
+    namaBarang: "Rossini",
+    jumlahStok: 36,
+    SKU: "RFrances@example.com",
+  },
+  {
+    id: 9,
+    harga: 10000,
+    namaBarang: "Harvey",
+    jumlahStok: 65,
+    SKU: "HRoxie@example.com",
+  },
 ];
 
 const TabelData = (props: ITabelDataProps) => {
-    const [searchText, setSearchText] = React.useState('');
-    const [rows, setRows] = React.useState<any[]>(originalRows);
+  const [searchText, setSearchText] = React.useState("");
+  const [rows, setRows] = React.useState<any[]>(originalRows);
 
-    const requestSearch = (searchValue: string) => {
-        setSearchText(searchValue);
-        const searchRegex = new RegExp(escapeRegExp(searchValue), 'i');
-        const filteredRows = originalRows.filter((row: any) => {
-            return Object.keys(row).some((field: any) => {
-                return searchRegex.test(row[field].toString());
-            });
-        });
-        setRows(filteredRows);
-    };
+  const requestSearch = (searchValue: string) => {
+    setSearchText(searchValue);
+    const searchRegex = new RegExp(escapeRegExp(searchValue), "i");
+    const filteredRows = originalRows.filter((row: any) => {
+      return Object.keys(row).some((field: any) => {
+        return searchRegex.test(row[field].toString());
+      });
+    });
+    setRows(filteredRows);
+  };
 
-    return (
-        <div style={{ height: 400, width: '100%' }}>
-            <DataGrid
-                editMode="row"
-                components={{
-                    Toolbar: QuickSearchToolbar
-                }}
-                rows={rows}
-                columns={columns}
-                pageSize={5}
-                rowsPerPageOptions={[5]}
-                checkboxSelection
-                componentsProps={{
-                    toolbar: {
-                        value: searchText,
-                        onChange: (event: React.ChangeEvent<HTMLInputElement>) =>
-                            requestSearch(event.target.value),
-                        clearSearch: () => requestSearch(''),
-                    },
-                }}
-            />
-        </div>
-    );
-}
+  return (
+    <div style={{ height: 400, width: "100%" }}>
+      <DataGrid
+        editMode="row"
+        components={{
+          Toolbar: QuickSearchToolbar,
+        }}
+        rows={rows}
+        columns={columns}
+        pageSize={5}
+        rowsPerPageOptions={[5]}
+        componentsProps={{
+          toolbar: {
+            value: searchText,
+            onChange: (event: React.ChangeEvent<HTMLInputElement>) =>
+              requestSearch(event.target.value),
+            clearSearch: () => requestSearch(""),
+          },
+        }}
+      />
+    </div>
+  );
+};
 
 export default TabelData;
