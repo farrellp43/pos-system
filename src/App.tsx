f002a159f984ed2093ca325321276f7716415b6a
farrellp43 <farrellp43@gmail.com>
2021-12-30 18:23:33 +0700

    menghilangkan arrow pada textfield number

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Routes from "./routes/Routes";
 import AppProvider from "./context";
+import "./App.css"
 
 function App() {
   return (
--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,12 +1,17 @@
 import React, { ReactNode } from "react";
 import { StokModalProvider } from "./stokModalContext";
+import { TransaksiProvider } from "./transaksiContext";
 
 interface IindexProps {
   children: ReactNode;
 }
 
 const AppProvider = ({ children }: IindexProps) => {
-  return <StokModalProvider>{children}</StokModalProvider>;
+  return (
+    <StokModalProvider>
+      <TransaksiProvider>{children}</TransaksiProvider>
+    </StokModalProvider>
+  );
 };
 
 export default AppProvider;
