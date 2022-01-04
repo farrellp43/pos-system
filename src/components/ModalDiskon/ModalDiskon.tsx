589caaed547c60800280b8bdb71171c1e421fe6b
farrellp43 <farrellp43@gmail.com>
2022-01-04 17:14:45 +0700

    add komponen modaldiskon (kosong)

---
+++ src/components/ModalDiskon/ModalDiskon.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface IModalDiskonProps {
+   
+}
+
+const ModalDiskon = (props: IModalDiskonProps) => {
+    return (
+        <div>
+           
+        </div>
+    )
+}
+
+export default ModalDiskon
