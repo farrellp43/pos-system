12e9047e73f7f1b402a5f0e083e5932dfa2bf8b4
farrellp43 <farrellp43@gmail.com>
2022-01-11 17:40:49 +0700

    change font

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,27 @@ import AppProvider from "./context";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import "./App.css";
 
+const font = "'Open Sans', sans-serif";
+
 const theme = createTheme({
+  shape: {
+    borderRadius: 8,
+  },
   palette: {
     primary: {
       main: "#45A779",
+      light: "#6AB893",
+      dark: "#307454",
       contrastText: "#fff",
     },
     secondary: {
       main: "#ffd600",
     },
+  },
+  typography: {
+    fontFamily: font,
+    fontWeightMedium: 500,
+    fontWeightBold: 700,
   },
 });
 
