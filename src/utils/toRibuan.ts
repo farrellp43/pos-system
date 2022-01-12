73fcf8494a49606baebfacc1f7d36452e72ed12c
farrellp43 <farrellp43@gmail.com>
2022-01-12 14:46:02 +0700

    add utils toRupiah and toRibuan

--- src/utils/toRibuan.ts
+++ src/utils/toRibuan.ts
@@ -1,6 +1,6 @@
 const toRibuan = (value: number) => {
     const num = value?.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1.")
-    return "Rp. " + num;
+    return num;
 };
 
 export default toRibuan;
