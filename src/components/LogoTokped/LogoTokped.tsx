15e12b945c11e3a21e9e5dc235b0aa0789e88ff3
farrellp43 <farrellp43@gmail.com>
2022-01-19 10:35:55 +0700

    fix bug: nested button tokopedia

--- src/components/LogoTokped/LogoTokped.tsx
+++ src/components/LogoTokped/LogoTokped.tsx
@@ -1,15 +1,10 @@
 import React from "react";
-import IconButton from "@mui/material/IconButton";
 import Tokopedia from "../../assets/icons/tokopedia.png";
 
 interface ILogoTokpedProps {}
 
 const LogoTokped = (props: ILogoTokpedProps) => {
-  return (
-    <IconButton disableRipple>
-      <img src={Tokopedia} alt="Tokopedia" height="22px" width="22px" />
-    </IconButton>
-  );
+  return <img src={Tokopedia} alt="Tokopedia" height="22px" width="22px" />;
 };
 
 export default LogoTokped;
