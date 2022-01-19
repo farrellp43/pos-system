aa9f4cf2a92ab9a5d93c9167a403601db9ea258d
farrellp43 <farrellp43@gmail.com>
2022-01-19 10:38:00 +0700

    wrap cookies provider pada app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,46 @@
 import React from "react";
 import Routes from "./routes/Routes";
 import AppProvider from "./context";
+import { CookiesProvider } from "react-cookie";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { idID } from "@mui/material/locale";
 import "./App.css";
 
 const font = "'Open Sans', sans-serif";
 
-const theme = createTheme({
-  shape: {
-    borderRadius: 8,
-  },
-  palette: {
-    primary: {
-      main: "#45A779",
-      light: "#6AB893",
-      dark: "#307454",
-      contrastText: "#fff",
+const theme = createTheme(
+  {
+    shape: {
+      borderRadius: 8,
     },
-    secondary: {
-      main: "#ffd600",
+    palette: {
+      primary: {
+        main: "#45A779",
+        light: "#6AB893",
+        dark: "#307454",
+        contrastText: "#fff",
+      },
+      secondary: {
+        main: "#ffd600",
+      },
+    },
+    typography: {
+      fontFamily: font,
+      fontWeightMedium: 500,
+      fontWeightBold: 700,
     },
   },
-  typography: {
-    fontFamily: font,
-    fontWeightMedium: 500,
-    fontWeightBold: 700,
-  },
-});
+  idID
+);
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <AppProvider>
-        <Routes />
-      </AppProvider>
+      <CookiesProvider>
+        <AppProvider>
+          <Routes />
+        </AppProvider>
+      </CookiesProvider>
     </ThemeProvider>
   );
 }
